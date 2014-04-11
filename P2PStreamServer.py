9422b04a5f7c334f100557431bc7517b45fd0a08
Hormazd Billimoria <hormazdb@gmail.com>
2014-04-11 04:50:04 -0400

    Audio and parse_launch string fix for Video streamer

--- P2PStreamServer.py
+++ P2PStreamServer.py
@@ -55,7 +55,8 @@ class StreamServer:
         return result
 
     def prepareStream(self):
-        self.player = Gst.parse_launch ("rtpbin name=rtpbin uridecodebin uri=file:///home/ankur/test_video2.mpeg name=decode decode. ! videoconvert ! videoscale ! videorate ! video/x-raw,width=352,height=288,framerate=15/1,pixel-aspect-ratio=1/1 ! videoconvert ! avenc_h263p ! rtph263ppay ! rtpbin.send_rtp_sink_0 rtpbin.send_rtp_src_0 ! queue ! udpsink host=127.0.0.1 port=5000 ts-offset=0 rtpbin.send_rtcp_src_0 ! udpsink host=127.0.0.1 port=5001 sync=false async=false udpsrc port=5005 ! rtpbin.recv_rtcp_sink_0 autoaudiosrc ! audioconvert ! amrnbenc ! rtpamrpay ! rtpbin.send_rtp_sink_1 rtpbin.send_rtp_src_1 ! queue ! udpsink host=127.0.0.1 port=5002 ts-offset=0 rtpbin.send_rtcp_src_1 ! udpsink host=127.0.0.1 port=5003 sync=false async=false udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1")
+        #self.player = Gst.parse_launch ("rtpbin name=rtpbin uridecodebin uri=file:///home/ankur/test_video2.mpeg name=decode decode. ! videoconvert ! videoscale ! videorate ! video/x-raw,width=352,height=288,framerate=15/1,pixel-aspect-ratio=1/1 ! videoconvert ! avenc_h263p ! rtph263ppay ! rtpbin.send_rtp_sink_0 rtpbin.send_rtp_src_0 ! queue ! udpsink host=127.0.0.1 port=5000 ts-offset=0 rtpbin.send_rtcp_src_0 ! udpsink host=127.0.0.1 port=5001 sync=false async=false udpsrc port=5005 ! rtpbin.recv_rtcp_sink_0 autoaudiosrc ! audioconvert ! amrnbenc ! rtpamrpay ! rtpbin.send_rtp_sink_1 rtpbin.send_rtp_src_1 ! queue ! udpsink host=127.0.0.1 port=5002 ts-offset=0 rtpbin.send_rtcp_src_1 ! udpsink host=127.0.0.1 port=5003 sync=false async=false udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1")
+        self.player = Gst.parse_launch(self.server_str);
 
     def startStream(self):
     # Move the Stream to PLAYING state
