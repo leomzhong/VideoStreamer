631382a1aa3a9eab78740364e60e73b2c556d001
Hormazd Billimoria <hormazdb@gmail.com>
2014-04-18 07:31:03 -0400

    Ported pyDHT to work with python3.3, Ported existing code to work with 3.3, Added GUI for client player which spawns on stream start.

---
+++ pydht_for py3.3/pydht/__init__.py
@@ -0,0 +1,2 @@
+from .pydht import DHT
+__version__ = "0.0.3"
---
+++ pydht_for py3.3/pydht/peer.py
@@ -0,0 +1,82 @@
+import hashlib
+import json
+
+from .hashing import hash_function
+
+class Peer(object):
+    ''' DHT Peer Information'''
+    def __init__(self, host, port, id):
+        self.host, self.port, self.id = host, port, id
+        
+    def astriple(self):
+        return (self.host, self.port, self.id)
+        
+    def address(self):
+        return (self.host, self.port)
+        
+    def __repr__(self):
+        return repr(self.astriple())
+
+    def _sendmessage(self, message, sock=None, peer_id=None, lock=None):
+        message["peer_id"] = peer_id # more like sender_id
+        encoded = bytes(json.dumps(message), 'UTF-8')
+        if sock:
+            if lock:
+                with lock:
+                    sock.sendto(encoded, (self.host, self.port))
+            else:
+                sock.sendto(encoded, (self.host, self.port))
+        
+    def ping(self, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "ping"
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def pong(self, socket=None, peer_id=None, lock=None):
+        message = {
+           "message_type": "pong"
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def store(self, key, value, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "store",
+            "id": key,
+            "value": value
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def find_node(self, id, rpc_id, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "find_node",
+            "id": id,
+            "rpc_id": rpc_id
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def found_nodes(self, id, nearest_nodes, rpc_id, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "found_nodes",
+            "id": id,
+            "nearest_nodes": nearest_nodes,
+            "rpc_id": rpc_id
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def find_value(self, id, rpc_id, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "find_value",
+            "id": id,
+            "rpc_id": rpc_id
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
+        
+    def found_value(self, id, value, rpc_id, socket=None, peer_id=None, lock=None):
+        message = {
+            "message_type": "found_value",
+            "id": id,
+            "value": value,
+            "rpc_id": rpc_id
+        }
+        self._sendmessage(message, socket, peer_id=peer_id, lock=lock)
